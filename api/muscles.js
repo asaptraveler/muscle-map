721896b0a28c993b7532e283076493b04ed6e7be
asaptraveler <creativepark.agency@gmail.com>
2025-03-28 18:19:56 +0100

    Update muscles.js

--- api/muscles.js
+++ api/muscles.js
@@ -2,9 +2,9 @@
 
 export default async function handler(req, res) {
   const NOTION_API_KEY = process.env.NOTION_API_KEY;
-  const NOTION_DB_ID = process.env.NOTION_DB_ID;
+  const NOTION_WORKOUT_DB_ID = process.env.NOTION_WORKOUT_DB_ID;
 
-  const response = await fetch(`https://api.notion.com/v1/databases/${NOTION_DB_ID}/query`, {
+  const response = await fetch(`https://api.notion.com/v1/databases/${NOTION_WORKOUT_DB_ID}/query`, {
     method: "POST",
     headers: {
       Authorization: `Bearer ${NOTION_API_KEY}`,
@@ -31,6 +31,6 @@ export default async function handler(req, res) {
 
   res.status(200).json({
     primary: [...primary],
-    secondary: [] // можно добавить потом
+    secondary: [] // можно будет дописать позже
   });
 }
