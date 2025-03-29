1db7237225b4093fd8d681597f0ad592d862243b
asaptraveler <creativepark.agency@gmail.com>
2025-03-29 01:29:42 +0100

    Update data.js

--- public/data.js
+++ public/data.js
@@ -1,1 +1,11 @@
+const exerciseDatabase = {
+  "Pull ups": ["lats", "biceps"],
+  "Deadlift": ["gluteus_maximus", "hamstrings", "lats"],
+  "Barbell Bench Press": ["pecs", "delts", "triceps"],
+  "Squat": ["quadriceps", "gluteus_maximus"],
+  "Overhead Press": ["delts", "triceps"],
+  "Push-ups": ["pecs", "triceps"]
+};
 
+let allData = [];
+let currentDisplayCount = 10;
